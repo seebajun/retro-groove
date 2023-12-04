a1e0777d8e68038eb35c1058f8ecb9c3a2dc969e
Guillermo Palma <gpalma.olea@gmail.com>
2023-12-04 20:22:38 -0300

    vista mobile producto y vender

--- src/views/Producto/Producto.jsx
+++ src/views/Producto/Producto.jsx
@@ -9,23 +9,21 @@ const Producto = () => {
   const { data, carrito, setCarrito, setTotal } = useContext(Context);
   const primerProducto = data[0];
 
-
-
   return (
     <>
       <NavB />
       <div className="contenedor">
         <div key={primerProducto.id} className="contenedorCard">
-          <div className="imgCard">
-            <img src={primerProducto.url} alt={primerProducto.nombre} />
-          </div>
           <div className="cardContent">
-            <h2>{primerProducto.nombre}</h2>
-            <h5>{`$${primerProducto.precio}.-`}</h5>
-            <h5>{primerProducto.descripcion}</h5>
-            <div className="precio">
-              <Button variant="primary">Agregar al 🛒</Button>
-              <Button variant="primary">❤</Button>
+            <img src={primerProducto.url} alt={primerProducto.nombre} />
+            <div className="cardText">
+              <h1>{primerProducto.nombre}</h1>
+              <h5>{`$${primerProducto.precio}.-`}</h5>
+              <h5>{primerProducto.descripcion}</h5>
+              <div className="botones">
+                <Button variant="primary">🛒</Button>
+                <Button variant="primary">❤</Button>
+              </div>
             </div>
           </div>
         </div>
