6fa5459ed3617c1364e69b438bdb600d6686fcd7
Hazel Guerra <hazelvguerra@gmail.com>
2023-12-04 19:25:14 -0300

    arreglo

--- src/views/Landing/landing.jsx
+++ src/views/Landing/landing.jsx
@@ -1,5 +1,5 @@
 import NavB from "../../components/Navbar/index";
-import './landing'
+import './landing.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
