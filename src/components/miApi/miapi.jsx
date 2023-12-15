1a3afb426f38b6e7868878cc143e5f3715ccd70b
Juan Aros <juaneduaros@gmail.com>
2023-12-15 18:53:30 -0300

    miapi y perfil consumen api

--- src/components/miApi/miapi.jsx
+++ src/components/miApi/miapi.jsx
@@ -4,53 +4,54 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import axios from "axios";
 // import Context from "../../context/index";
 
 function Posts() {
     // mostrar landing2 en cards
-    // const { productos } = useContext(Context);
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+    const [posts, setPosts] = useState([]);
+    // const { user } = useContext(Context);
+    const token = localStorage.getItem("token");
+    console.log("Token:", token);
+    const urlServer = "http://localhost:2999";
+    const endpoint = "/landing2";
+    console.log("Token:", token);
+    const handleGetPosts = async () => {
+        try {
+            const response = await axios.get(urlServer + endpoint, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log("response", response.data);
+            setPosts(response.data);
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+            alert("Hubo un error al cargar los productos. Por favor, inténtalo de nuevo. 🙁");
+        }
+    };
     useEffect(() => {
-        setLoading(true);
-        setError(false);
-        const getProducts = async () => {
-            try {
-                const response = await fetch("localhost:3000/landing2/");
-                const data = await response.json();
-                setProducts(data);
-            } catch (error) {
-                setError(true);
-            }
-            setLoading(false);
-        };
-        getProducts();
-    }, []);
-    
-
+        handleGetPosts();
+    }, []); 
     return (
-        <div>
-            <Container>
-                <Row>
-                    {loading && <p>Cargando...</p>}
-                    {error && <p>Hubo un error</p>}
-                    {products.map((product) => (
-                        <Col xs={12} sm={6} md={4} lg={3} key={product.id}>
-                            <Card>
-                                <Card.Img variant="top" src={product.image} />
-                                <Card.Body>
-                                    <Card.Title>{product.title}</Card.Title>
-                                    <Card.Text>{product.description}</Card.Text>
-                                    <Button variant="primary">Ver más</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
-         
+        <Container>
+            <Row>
+                {posts.map((post) => (
+                    <Col key={post._id} xs={12} sm={6} md={4} lg={3}>
+                        <Card className="card">
+                            <Card.Img variant="top" src={post.imagen} />
+                            <Card.Body>
+                                <Card.Title>{post.titulo}</Card.Title>
+                                <Card.Text>{post.formato}</Card.Text>
+                                <Card.Text>{post.precio}</Card.Text>
+                                <Button variant="primary">Comprar</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
 
--- src/views/Perfil/Perfil.jsx
+++ src/views/Perfil/Perfil.jsx
@@ -1,88 +1,66 @@
 import NavB from "../../components/Navbar/navbar";
 import Form from 'react-bootstrap/Form';
 import './perfil.css'
-import { Container, Col, Row, Card, TabContainer } from "react-bootstrap";
+import { Container, Col, Row, Card, TabContainer, Nav } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Perfil = () =>{
-    return(
+            //consumir api de usuario
+        const [user, setUser] = useState({
+            nombre: "",
+            apellido: "",
+            email: ""});
+        const token = localStorage.getItem("token");
+        console.log("Token:", token);
+        const urlServer = "http://localhost:2999";
+        const endpoint = "/perfil";
+        console.log("Token:", token);
+        const handleGetUser = async () => {
+            try {
+                const response = await axios.get(urlServer + endpoint, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                console.log("response", response.data);
+                setUser(response.data);
+            } catch (error) {
+                console.error("Error en la solicitud:", error);
+                alert("Hubo un error al cargar los productos. Por favor, inténtalo de nuevo. 🙁");
+            }    
         
-        <>
-        <NavB />
-        <Container fluid className="miContenedor">
-        <Row xs={1} lg={3} className="d-flex justify-content-center">
-            <Col>
-                <Card className="cardPerfil">
-                    <h4>Mi perfil</h4>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>Nombre</Form.Label>
-                            <Form.Control type="text" placeholder="Mi nombre" readOnly disabled className="mb-3"/>
-                            </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Apellido</Form.Label>
-                            <Form.Control type="text" placeholder="Mi apellido" readOnly disabled className="mb-3"/>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="text" placeholder="Mi email" readOnly disabled className="mb-3"/>
-                        </Form.Group>
-                    </Form>
-                </Card>
-            </Col>
-            <Col>
-                <Card className="cardPerfil">
-                    <h4>Vender un articulo en Retro Groove</h4>
-                    <p>Con Retro Groove puedes publicar y vender tus albumes de vinilos, CD o cassetes, de manera rapida, confiable y 100% gratis.</p>
-                    <Button className="w-50" variant="danger" href="/vender">Ir a vender</Button>
-                </Card>
-            </Col>
-        </Row>
-        <Row>
-            <Col className="d-flex justify-content-center">
-                <Container>
-                    <h4 className="mb-4">Mis articulos publicados</h4>
-                    <Row xs="1" lg="3">
-                        <Col>
-                        <Card className="cardPerfil ">
-                        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgbaAA2G6opkLTTtKqkQ-ENplIhZFeJpHoH39SKK1KY0y2RqS_swET8IJqkrEFUW_lfiw&usqp=CAU" height={200}/>
-                        <Card.Body>
-                            <Card.Title></Card.Title>
-                            <Card.Text className="fw-bold fs-5">Producto1</Card.Text>
-                            <Button variant="danger" href="/producto">Modificar este articulo</Button>
-                        </Card.Body>
+        };
+        useEffect(() => {
+            handleGetUser();
+        }, []);
+        return(
+            <>
+            <NavB />
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Perfil</Card.Title>
+                                <Card.Text>{user.nombre}</Card.Text>
+                                <Card.Text>{user.apellido}</Card.Text>
+                                <Card.Text>{user.email}</Card.Text>
+                                <Button variant="primary">Editar</Button>
+                            </Card.Body>
                         </Card>
-                        </Col>
-                        <Col>
-                        <Card className="cardPerfil">
-                        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgbaAA2G6opkLTTtKqkQ-ENplIhZFeJpHoH39SKK1KY0y2RqS_swET8IJqkrEFUW_lfiw&usqp=CAU" height={200}/>
-                        <Card.Body>
-                            <Card.Title></Card.Title>
-                            <Card.Text className="fw-bold fs-5">Producto2</Card.Text>
-                            <Button variant="danger" href="/producto">Modificar este articulo</Button>
-                        </Card.Body>
-                    </Card>
-                        </Col>
-                        <Col>
-                        <Card className="cardPerfil">
-                        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgbaAA2G6opkLTTtKqkQ-ENplIhZFeJpHoH39SKK1KY0y2RqS_swET8IJqkrEFUW_lfiw&usqp=CAU" height={200}/>
-                        <Card.Body>
-                            <Card.Title></Card.Title>
-                            <Card.Text className="fw-bold fs-5">Producto3</Card.Text>
-                            <Button variant="danger" href="/producto">Modificar este articulo</Button>
-                        </Card.Body>
-                    </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </Col>
-        </Row>
-        </Container>
-        <div className="bg-dark text-light text-center p-4">
-            <h5>Todos los derechos reservados.</h5>
-        </div>
-        </>
-    );
+                    </Col>
+                </Row>
+            </Container>
+            {/* footer con redes sociales */}
+            <div></div>
+            </>
+            
+            
+            
+        );
 };
 
 export default Perfil;
